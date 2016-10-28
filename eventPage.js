f07afb3fb7cf2ae3bc5da6b6d45769f5453c599a
chechengpeng <chechengpeng@126.com>
2016-10-28 14:38:46 +0800

    some test

--- eventPage.js
+++ eventPage.js
@@ -1,4 +1,26 @@
-'use strict';
 /**
  * Created by che on 2016/10/25.
  */
+var sites = 'abc',tabId = '';
+chrome.browserAction.onClicked.addListener(function(tab) {
+  var viewTabUrl = chrome.extension.getURL('popup.html');
+  var views = chrome.extension.getViews();
+  console.log(views);
+  console.log(viewTabUrl);
+  if(views[0].location.href == viewTabUrl){
+    views[0].setSites(sites);
+  }
+  chrome.tabs.create({url:"popup.html"});
+});
+
+chrome.tabs.onCreated.addListener(function(tab){
+  //sites = tab.url+'-'+sites;
+});
+
+chrome.tabs.onActivated.addListener(function (tab){
+  chrome.tabs.get(tab.tabId, function(tab){
+    sites = tab.url+'<br>'+sites;
+  })
+});
+
+
--- findUrl.js
+++ findUrl.js
@@ -2,20 +2,31 @@
 /**
  * Created by che on 2016/10/25.
  */
+/*
 function getTabUrl(){
   var queryInfo = {
-    url:'https://*.zhihu.com/'
-  };
-  chrome.tabs.query(queryInfo, function(tabs) {
-    if(tabs.length>0){
-      document.getElementById('show').textContent = '你浏览器打开着知乎';
-      console.log(tabs);
-    }else{
-      document.getElementById('show').textContent = '你没有看知乎';
+    url:'https://!*.zhihu.com/'
+  },sites = 'abc';
+  chrome.tabs.onCreated.addListener(function(tab){
+      sites = tab.id + sites;
+  });
+  document.getElementById('show').innerHTML = sites;
+
+
+  /!*
+   * 获取当前打开的tab页的url
+  chrome.tabs.query({}, function(tabs) {
+    for(var i=0;i<tabs.length;i++){
+      sites = tabs[i].url +'<br>'+sites;
     }
-  })
+    document.getElementById('show').innerHTML = sites;
+  })*!/
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   getTabUrl();
-});
+});*/
+
+function setSites(sites){
+  document.getElementById('show').innerHTML = sites;
+}
